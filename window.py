49f9b5ae615316758694f76dd90cf5a88b752f95
u220110 <ryan.smith.2022@mumail.ie>
2024-09-25 11:18:47 +0100

    draws points

--- window.py
+++ window.py
@@ -57,7 +57,7 @@ class MyCursor:
             glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT) #Clear window
             # if(myCursor.myCursor==True):
             #     print("y")
-            glColor3f(1,0,0)
-            glBegin(GL_POINTS)
-            glVertex2f(100,50)
-            glEnd()
+            # glColor3f(1,0,0)
+            # glBegin(GL_POINTS)
+            # glVertex2f(100,50)
+            # glEnd()
